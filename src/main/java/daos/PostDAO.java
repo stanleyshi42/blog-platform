81e9234b6edbac38aed148e0b711ddd04f88b721
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-05 16:04:00 -0700

    added getting post by id

--- src/main/java/daos/PostDAO.java
+++ src/main/java/daos/PostDAO.java
@@ -4,6 +4,7 @@ import com.mongodb.client.*;
 import documents.Post;
 import documents.User;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 
@@ -80,6 +81,37 @@ public class PostDAO {
 
         mongoClient.close();
         return posts;
+    }
+
+    public static Post getPostById(String id) {
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = mongoClient.getDatabase("blogPlatform");
+        MongoCollection<Document> collection = database.getCollection("posts");
+
+        Document filter = new Document("_id", new ObjectId(id));
+        FindIterable<Document> result = collection.find(filter);
+
+        // Check if operation was successful
+        if (!result.iterator().hasNext())
+            return null;
+
+        for (Document d : result) {
+            String username = d.get("username").toString();
+            String text = d.get("text").toString();
+            ArrayList<String> tags = (ArrayList<String>) d.get("tags");
+
+            Post post = new Post(id, username, text, 0, tags, new ArrayList<>());
+            System.out.println(post); // TODO delete debug text
+            return post;
+        }
+
+        mongoClient.close();
+        return null;
+    }
+
+    public static boolean updateLikes(int likes) {
+
+        return false;
     }
 
 }
